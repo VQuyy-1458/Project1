848700ba639210525361e7c5797f5884d96ff47a
VQuyy-1458 <nguyenthuavanquy@gmail.com>
2025-11-19 14:40:41 +0700

    Update PlayerService.java

--- src/Service/PlayerService.java
+++ src/Service/PlayerService.java
@@ -1,4 +1,45 @@
 package Service;
 
-public class PlayerService {
+import dao.PlayerDAO;
+import model.Player;
+import service.interface.IPlayerService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerService implements IPlayerService {
+
+    private final PlayerDAO dao = new PlayerDAO();
+    private final String filename = "CT.DAT";
+    private List<Player> players;
+
+    public PlayerService() {
+        players = dao.readFile(filename);
+        if (players == null) players = new ArrayList<>();
+    }
+
+    @Override
+    public void add(Player p) {
+        players.add(p);
+        dao.writeFile(filename, players);
+    }
+
+    @Override
+    public List<Player> getAll() {
+        return players;
+    }
+
+    @Override
+    public Player findById(int id) {
+        for (Player p : players)
+            if (p.getId() == id)
+                return p;
+        return null;
+    }
+
+    @Override
+    public int generateId() {
+        if (players.isEmpty()) return 100;
+        return players.get(players.size() - 1).getId() + 1;
+    }
 }
